9f7927dbbc595b26072ef0e15801875e647e7871
Connor <connor.zhao@outlook.com>
2026-03-04 20:30:29 +1100

    Init project

--- .rnstorybook/storybook.requires.ts
+++ .rnstorybook/storybook.requires.ts
@@ -11,12 +11,12 @@ const normalizedStories = [
     titlePrefix: "",
     directory: "./components",
     files: "**/*.stories.?(ts|tsx|js|jsx)",
-    importPathMatcher: /^\.(?:(?:^|\/|(?:(?:(?!(?:^|\/)\.).)*?)\/)(?!\.)(?=.)[^/]*?\.stories\.(?:ts|tsx|js|jsx)?)$/,
+    importPathMatcher: /^\.(?:(?:^|[\\/]|(?:(?:(?!(?:^|[\\/])\.).)*?)[\\/])(?!\.)(?=.)[^\\/]*?\.stories\.(?:ts|tsx|js|jsx)?)$/,
     // @ts-ignore
     req: require.context(
       '../components',
       true,
-      /^\.(?:(?:^|\/|(?:(?:(?!(?:^|\/)\.).)*?)\/)(?!\.)(?=.)[^/]*?\.stories\.(?:ts|tsx|js|jsx)?)$/
+      /^\.(?:(?:^|[\\/]|(?:(?:(?!(?:^|[\\/])\.).)*?)[\\/])(?!\.)(?=.)[^\\/]*?\.stories\.(?:ts|tsx|js|jsx)?)$/
     ),
   }
 ];
@@ -25,6 +25,7 @@ const normalizedStories = [
 declare global {
   var view: View;
   var STORIES: typeof normalizedStories;
+  var STORYBOOK_WEBSOCKET: { host: string; port: number } | undefined;
 }
 
 
@@ -33,21 +34,22 @@ const annotations = [
   require("@storybook/react-native/preview")
 ];
 
-global.STORIES = normalizedStories;
+globalThis.STORIES = normalizedStories;
+
 
 // @ts-ignore
 module?.hot?.accept?.();
 
 
 
-if (!global.view) {
-  global.view = start({
+if (!globalThis.view) {
+  globalThis.view = start({
     annotations,
     storyEntries: normalizedStories,
 
   });
 } else {
-  updateView(global.view, annotations, normalizedStories);
+  updateView(globalThis.view, annotations, normalizedStories);
 }
 
-export const view: View = global.view;
+export const view: View = globalThis.view;
